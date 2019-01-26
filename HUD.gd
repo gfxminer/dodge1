f62844a6f27e853e516702cd8d6ae9ff76b2b23a
Adrian Nungka <adrian@theadx.net>
2019-01-27 00:59:51 +0800

    Godot - Your First Game Implementation

---
+++ HUD.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+    $MessageLabel.text = text
+    $MessageLabel.show()
+    $MessageTimer.start()
+
+func show_game_over():
+    show_message("Game Over")
+    yield($MessageTimer, "timeout")
+    $StartButton.show()
+    $MessageLabel.text = "Dodge the\nCreeps!"
+    $MessageLabel.show()
+    #yield(get_tree().create_timer(1), 'timeout')
+
+
+func update_score(score):
+    $ScoreLabel.text = str(score)
+
+
+func _on_StartButton_pressed():
+    $StartButton.hide()
+    emit_signal("start_game")
+
+func _on_MessageTimer_timeout():
+    $MessageLabel.hide()
